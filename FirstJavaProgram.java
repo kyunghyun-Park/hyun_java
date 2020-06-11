b00f3835b2ebff8c2f7cb96e9f2d3f20e5e5d141
hyun <50633008+kyunghyun-Park@users.noreply.github.com>
2020-06-11 23:26:03 +0900

    zz

--- FirstJavaProgram.java
+++ FirstJavaProgram.java
@@ -25,7 +25,7 @@ class BankAccount {
 }
 public class FirstJavaProgram {
     public static void main(String[] args) {
-        BankAccount park = new BankAccount("12-23-23", "961211-2222222", 10000);
+        BankAccount park = new BankAccount("12-23-23", "96121112-2222222", 10000);
 
 
         BankAccount hong = new BankAccount("23-23-23", "990517-1111111", 20000);
