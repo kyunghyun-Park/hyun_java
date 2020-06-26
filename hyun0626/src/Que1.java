1a9e972d6c337b1c8ff95cf5c41116fad56d2cd1
kyunghyun-park <qqqq_o@naver.com>
2020-06-26 16:33:38 +0900

    자바문제

--- hyun0626/src/Que1.java
+++ hyun0626/src/Que1.java
@@ -2,11 +2,28 @@ import java.util.Scanner;
 
 public class Que1 {
     public static void main(String[] args) {
+        int[] cnt = new int[26];
         String name = "";
 
         Scanner sc = new Scanner(System.in);
         name = sc.nextLine();
 
+        for(int i=0;i<name.length();i++){
+            //각 문자열을 ch로 따옴
+            char ch = name.charAt(i);
+            //a(97)-a(97) = 0 이므로 cnt[0]자리에 ++
+            cnt[ch -'a']++;
+        }
+//        for(int i=0;i<26;i++)
+//            System.out.print(" "+cnt[i]);
+
+        System.out.println("\n각 문자의 수");
+
+        for(int i=0;i<26;i++){
+            //각 자리에 1이 올라간 인덱스만 추출
+            if(cnt[i]!=0)
+                System.out.println((char)(97+i)+":"+cnt[i]);
+        }
 
     }
 }
--- hyun0626/src/Que2.java
+++ hyun0626/src/Que2.java
@@ -11,11 +11,10 @@ public class Que2 {
         int len=array.length;
         int[] count = new int[len];
 
-        for (int i = 0; i < array.length; ) {
-            System.out.println("array.length = "+array.length);
-            while(array[i].equals(array2[i])){
+        for (int i = 0; i < array.length;i++ ) {
+            char tmp = str.charAt(i);
 
-            }
+
         }
 
 //        for (int i = 0; i < str.length(); i++) {
