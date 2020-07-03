3e3312188dd89dc6586ead7ae3ba2b4850d9b1b3
kyunghyun-park <qqqq_o@naver.com>
2020-07-03 15:51:02 +0900

    toString 오버라이딩

---
+++ hjava/inheritance/src/OverridingToString.java
@@ -0,0 +1,29 @@
+class Cake {
+    //Object 클래스의 toString 메소드를 오버라이딩
+    public String toString(){
+        //Object 클래스의 toString 메소드 호출하여 반환 결과 출력
+        System.out.println(super.toString());
+        return "My birthday cake";
+    }
+}
+
+class CheeseCake extends Cake {
+    //Cake 클래스의 toString 메소드를 오버라이딩
+    public String toString(){
+        return "My birthday cheese cake";
+    }
+}
+
+ class OverridingToString {
+     public static void main(String[] args) {
+         Cake c1 = new Cake();
+         Cake c2 = new CheeseCake();
+
+         //c1이 참조하는 인스턴스의 toString 메소드 호출로 이어짐
+         System.out.println(c1);
+         System.out.println();
+
+         //c2가 참조하는 인스턴스의 toString 메소드 호출로 이어짐
+         System.out.println(c2);
+     }
+}
