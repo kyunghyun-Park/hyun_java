53b242f4233c1341c2b7a874886e4211cf1deff5
kyunghyun-park <qqqq_o@naver.com>
2020-07-13 17:13:11 +0900

    별 찍기

---
+++ hjava/backjoon/src/stage5/B10996.java
@@ -0,0 +1,15 @@
+package stage5;
+
+import java.util.Scanner;
+
+public class B10996 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+
+        if(n==1){
+            System.out.println("*");
+        }
+
+    }
+}
