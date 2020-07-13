9e1dc245b44b655e1b0646709b3fc58d2a3ee32d
hyun <50633008+kyunghyun-Park@users.noreply.github.com>
2020-07-14 01:04:58 +0900

    별 찍기

--- hjava/backjoon/src/stage5/B10996.java
+++ hjava/backjoon/src/stage5/B10996.java
@@ -1,7 +1,34 @@
 package stage5;
 
+import java.util.Scanner;
+
 public class B10996 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
 
+        for (int i = 0; i <= 2 * n; i++) {
+                //짝수 행
+            if (i % 2 == 0) {
+                for (int j = 1; j <= n; j++) {
+                    //홀수 번째
+                    if (j % 2 == 1) {
+                        System.out.print("*");
+                    } else {
+                        System.out.print(" ");
+                    }
+                }
+                //홀수 행
+            } else {
+                for (int j = 1; j <= n; j++) {
+                    if (j % 2 == 1) {
+                        System.out.print(" ");
+                    } else {
+                        System.out.print("*");
+                    }
+                }
+            }
+            System.out.println();
+        }
     }
 }
