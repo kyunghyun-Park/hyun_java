1f113920a33da5f93514deebd59ed42d0ef85ff5
kyunghyun-park <qqqq_o@naver.com>
2020-07-13 12:09:13 +0900

    abcd -> ㄱㄴㄷㄹ

--- hjava/que0630/src/test2/Que9.java
+++ hjava/que0630/src/test2/Que9.java
@@ -6,15 +6,18 @@ public class Que9 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
-
+        StringBuilder result = new StringBuilder();
         char[] hangul = new char[]{
                 'ㄱ', 'ㄴ', 'ㄷ', 'ㄹ', 'ㅁ', 'ㅂ', 'ㅅ', 'ㅇ', 'ㅈ', 'ㅊ',
                 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ', 'ㅏ', 'ㅑ', 'ㅓ', 'ㅕ', 'ㅗ', 'ㅛ', 'ㅜ',
                 'ㅠ', 'ㅡ', 'ㅣ'};
 
-        System.out.println((int)str.charAt(0));
-        for (int i = 0; i < str.length(); i++)
+        System.out.println((int) str.charAt(0));
+        for (int i = 0; i < str.length(); i++) {
+            result.append(hangul[str.charAt(i) - (int) 'a']);
             System.out.print(hangul[str.charAt(i) - (int) 'a']);
-
+        }
+        System.out.println();
+        System.out.println(result);
     }
 }
