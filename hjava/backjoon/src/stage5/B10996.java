c4358edf25e0394d8dca87814c8947853b639db3
hyun <50633008+kyunghyun-Park@users.noreply.github.com>
2020-07-14 00:27:20 +0900

    Update B10996.java

--- hjava/backjoon/src/stage5/B10996.java
+++ hjava/backjoon/src/stage5/B10996.java
@@ -1,7 +1,21 @@
 package stage5;
 
+<<<<<<< HEAD
 public class B10996 {
     public static void main(String[] args) {
         
+=======
+import java.util.Scanner;
+
+public class B10996 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+
+        if(n==1){
+            System.out.println("*");
+        }
+
+>>>>>>> 53b242f4233c1341c2b7a874886e4211cf1deff5
     }
 }
