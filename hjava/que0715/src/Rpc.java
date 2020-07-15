61a8dce0afa3e3c95d0bba068f41366f32f7de12
kyunghyun-park <qqqq_o@naver.com>
2020-07-15 11:18:21 +0900

    가위바위보

---
+++ hjava/que0715/src/Rpc.java
@@ -0,0 +1,41 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Rpc {
+    static String getRandom() {
+        Random random = new Random();
+        String arr[] = {"보", "바위", "가위"};
+
+        int randNum = random.nextInt(arr.length);
+
+        return arr[randNum];
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String me = sc.nextLine();
+
+        String b = getRandom();
+        System.out.println(b);
+
+        if (me.equals("가위")) {
+            if (b.equals("바위"))
+                System.out.println("졌음");
+            else if (b.equals("보"))
+                System.out.println("이겼음");
+            else System.out.println("비김");
+        } else if (me.equals("바위")) {
+            if (b.equals("보"))
+                System.out.println("졌음");
+            else if (b.equals("가위"))
+                System.out.println("이겼음");
+            else System.out.println("비김");
+        } else if (me.equals("보")) {
+            if (b.equals("가위"))
+                System.out.println("졌음");
+            else if (b.equals("바위"))
+                System.out.println("이겼음");
+            else System.out.println("비김");
+        }
+    }
+}
