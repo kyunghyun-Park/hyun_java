3988873426b32442f1fa2bf38cd1d284e1f1c19d
kyunghyun-park <qqqq_o@naver.com>
2020-07-15 11:34:10 +0900

    설탕공장

--- hjava/backjoon/src/stage9/B2839_while2.java
+++ hjava/backjoon/src/stage9/B2839_while2.java
@@ -7,19 +7,19 @@ import java.io.InputStreamReader;
 public class B2839_while2 {
     public static void main(String[] args) throws NumberFormatException, IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(reader.readLine());
+        int n = Integer.parseInt(reader.readLine());
         int count = 0;
 
-        while(N%5 != 0 && N > 0){
-            N -= 3;
+        while(n%5 != 0 && n > 0){
+            n -= 3;
             count++;
-            System.out.println("N="+N);
+            System.out.println("n="+n);
             System.out.println("count="+count);
         }
 
-        if(N<0)
+        if(n<0)
             System.out.println(-1);
         else
-            System.out.println(count + N/5);
+            System.out.println(count + n/5);
     }
 }
