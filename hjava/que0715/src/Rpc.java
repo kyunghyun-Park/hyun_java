3fbe88058ac9f3e10c96808e74c3d0b78e9ebc75
kyunghyun-park <qqqq_o@naver.com>
2020-07-15 11:19:08 +0900

    가위바위보

--- hjava/que0715/src/Rpc.java
+++ hjava/que0715/src/Rpc.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Rpc {
     static String getRandom() {
         Random random = new Random();
-        String arr[] = {"보", "바위", "가위"};
+        String[] arr = {"보", "바위", "가위"};
 
         int randNum = random.nextInt(arr.length);
 
@@ -18,24 +18,48 @@ public class Rpc {
         String b = getRandom();
         System.out.println(b);
 
-        if (me.equals("가위")) {
-            if (b.equals("바위"))
-                System.out.println("졌음");
-            else if (b.equals("보"))
-                System.out.println("이겼음");
-            else System.out.println("비김");
-        } else if (me.equals("바위")) {
-            if (b.equals("보"))
-                System.out.println("졌음");
-            else if (b.equals("가위"))
-                System.out.println("이겼음");
-            else System.out.println("비김");
-        } else if (me.equals("보")) {
-            if (b.equals("가위"))
-                System.out.println("졌음");
-            else if (b.equals("바위"))
-                System.out.println("이겼음");
-            else System.out.println("비김");
+        switch (me) {
+            case "가위":
+                if (b.equals("바위"))
+                    System.out.println("졌음");
+                else if (b.equals("보"))
+                    System.out.println("이겼음");
+                else System.out.println("비김");
+                break;
+            case "바위":
+                if (b.equals("보"))
+                    System.out.println("졌음");
+                else if (b.equals("가위"))
+                    System.out.println("이겼음");
+                else System.out.println("비김");
+                break;
+            case "보":
+                if (b.equals("가위"))
+                    System.out.println("졌음");
+                else if (b.equals("바위"))
+                    System.out.println("이겼음");
+                else System.out.println("비김");
+                break;
         }
+
+//        if (me.equals("가위")) {
+//            if (b.equals("바위"))
+//                System.out.println("졌음");
+//            else if (b.equals("보"))
+//                System.out.println("이겼음");
+//            else System.out.println("비김");
+//        } else if (me.equals("바위")) {
+//            if (b.equals("보"))
+//                System.out.println("졌음");
+//            else if (b.equals("가위"))
+//                System.out.println("이겼음");
+//            else System.out.println("비김");
+//        } else if (me.equals("보")) {
+//            if (b.equals("가위"))
+//                System.out.println("졌음");
+//            else if (b.equals("바위"))
+//                System.out.println("이겼음");
+//            else System.out.println("비김");
+//        }
     }
 }
