844f452a8ae8ea0a20e0d52a600d226f7053617c
kyunghyun-park <qqqq_o@naver.com>
2020-07-15 16:33:29 +0900

    셀프 넘버

--- hjava/backjoon/src/stage7/B4673.java
+++ hjava/backjoon/src/stage7/B4673.java
@@ -1,6 +1,19 @@
 package stage7;
 
-
-
 public class B4673 {
+    static void selfnumber() {
+        int n = 100;
+        for (int i = 1; i <= 100; i++) {
+            int b = i + (i / 10) + (i % 10);
+            System.out.println(b);
+            for(int j=b;j<=100;j++){
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        selfnumber();
+
+    }
 }
