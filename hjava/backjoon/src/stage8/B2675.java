a44ea8d37267ce7e5d299d889dc6a4ec8d632882
hyun <50633008+kyunghyun-Park@users.noreply.github.com>
2020-07-16 22:27:06 +0900

    3 abc

--- hjava/backjoon/src/stage8/B2675.java
+++ hjava/backjoon/src/stage8/B2675.java
@@ -5,23 +5,35 @@ import java.util.Scanner;
 public class B2675 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String str = sc.nextLine();
+        int n = sc.nextInt();
+        sc.nextLine(); //버퍼비우기
 
-        String[] arr = str.split((" "));
+        String[] x = new String[n];
+        String[] arr = new String[n*2];
 
-        String[] arr2 = arr[1].split("");
-
-
-        int cnt = Integer.parseInt(arr[0]);
-        System.out.println(cnt);
-        int c = 0;
-
-        for (int i = 0; i <  arr2.length; i++) {
-            for (int j = 0; j <cnt; j++) {
-                System.out.print(arr2[c]);
-            }
-            c++;
+        for (int i = 0; i < n; i++) {
+            x[i] = sc.nextLine(); //3 ABC,5 /HTP
         }
+        System.out.println(arr.length);
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
+
+
+
+//        String[] arr2 = arr[1].split("");
+//
+//
+//        int cnt = Integer.parseInt(arr[0]);
+//        System.out.println(cnt);
+//        int c = 0;
+//
+//        for (int i = 0; i < arr2.length; i++) {
+//            for (int j = 0; j < cnt; j++) {
+//                System.out.print(arr2[c]);
+//            }
+//            c++;
+//        }
 
 
     }
