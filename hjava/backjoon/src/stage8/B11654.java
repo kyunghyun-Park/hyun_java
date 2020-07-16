1e2b34797bfbe8be378f19fdbbcac05b11cb2f04
kyunghyun-park <qqqq_o@naver.com>
2020-07-16 16:31:00 +0900

    아스키코드 출력

---
+++ hjava/backjoon/src/stage8/B11654.java
@@ -0,0 +1,13 @@
+package stage8;
+
+import java.util.Scanner;
+
+public class B11654 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        char b = sc.nextLine().charAt(0);
+        System.out.println((int)b);
+
+    }
+}
