f3dd0d5d15dd5f532881ef445c1766783fc2ea00
kyunghyun-park <qqqq_o@naver.com>
2020-07-17 09:19:59 +0900

    셀프 넘버

--- hjava/backjoon/src/stage7/B4673.java
+++ hjava/backjoon/src/stage7/B4673.java
@@ -1,19 +1,29 @@
 package stage7;
 
 public class B4673 {
-    static void selfnumber() {
-        int n = 100;
-        for (int i = 1; i <= 100; i++) {
-            int b = i + (i / 10) + (i % 10);
-            System.out.println(b);
-            for(int j=b;j<=100;j++){
-            }
+    public static void main(String[] args) {
+
+        int[] arr = new int[10000];
+
+        for (int n = 1; n < arr.length; n++) {
+            if (d(n)>=10000)
+                continue;
+
+            arr[d(n)]=1;
         }
 
+        for (int n = 1; n < arr.length; n++) {
+            if (arr[n] == 0)
+                System.out.println(n);
+        }
     }
 
-    public static void main(String[] args) {
-        selfnumber();
+    public static int d(int n) {
+        int x = n / 1000;
+        int y = (n % 1000) / 100;
+        int q = (n % 100) / 10;
+        int w = n % 10;
 
+        return n + x + y + q + w;
     }
 }
