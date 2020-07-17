11a08e0dda1031646f54b984a51ed32107e21e06
kyunghyun-park <qqqq_o@naver.com>
2020-07-17 11:41:17 +0900

    2 3 ABC 5 /HTP
    BufferedReader.readLine , split 사용

--- hjava/backjoon/src/stage8/B2675.java
+++ hjava/backjoon/src/stage8/B2675.java
@@ -1,40 +1,33 @@
 package stage8;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class B2675 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        sc.nextLine(); //버퍼비우기
+    public static void main(String[] args) throws IOException {
 
-        String[] x = new String[n];
-        String[] arr = new String[n*2];
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
 
         for (int i = 0; i < n; i++) {
-            x[i] = sc.nextLine(); //3 ABC,5 /HTP
+            String[] arr = br.readLine().split(" ");    //바로 띄어쓰기 기준 3과 abc를 각 arr[0] , arr[1] 에 넣음
+
+            int cnt = Integer.parseInt(arr[0]);               //arr[0]을 숫자로 변환
+
+            //String[] str = arr[1].split("");              //배열쓰는방법
+            String str = arr[1];                             //abc를 통째로 넣고
+
+            for (int j = 0; j < str.length(); j++) {           //글자 개수를
+                for (int k = 0; k < cnt; k++) {                 //카운트만큼 돌림
+                    // System.out.print(str[j]);            //배열쓰는방법
+                    System.out.print(str.charAt(j));
+                }
+
+            }
+            System.out.println();
         }
-        System.out.println(arr.length);
-        for(int i=0;i<arr.length;i++){
-            System.out.println(arr[i]);
-        }
-
-
-
-//        String[] arr2 = arr[1].split("");
-//
-//
-//        int cnt = Integer.parseInt(arr[0]);
-//        System.out.println(cnt);
-//        int c = 0;
-//
-//        for (int i = 0; i < arr2.length; i++) {
-//            for (int j = 0; j < cnt; j++) {
-//                System.out.print(arr2[c]);
-//            }
-//            c++;
-//        }
-
 
     }
 }
