c9b83380938128040f852a699bfe9c4400699f2b
kyunghyun-park <qqqq_o@naver.com>
2020-09-21 16:36:02 +0900

    Create SessionTest.java

---
+++ hjsp/0921/src/SessionTest.java
@@ -0,0 +1,36 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+@WebServlet("/sessionTest")
+public class SessionTest extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+    public SessionTest() {
+        super();
+    }
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		session.setAttribute("name", "하이");
+		response.setContentType("text/html;charset=UTF-8");
+		PrintWriter out=response.getWriter();
+		out.println("<h1>이름저장</h1>");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
