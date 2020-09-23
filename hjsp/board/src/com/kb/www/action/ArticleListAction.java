2f66a776e92ed219e20b48026fdbf793b09e3942
kyunghyun-park <qqqq_o@naver.com>
2020-09-23 16:42:22 +0900

    Update ArticleListAction.java

--- hjsp/board/src/com/kb/www/action/ArticleListAction.java
+++ hjsp/board/src/com/kb/www/action/ArticleListAction.java
@@ -7,33 +7,16 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.kb.www.common.Action;
 import com.kb.www.common.ActionForward;
+import com.kb.www.service.BoardService;
 import com.kb.www.vo.ArticleVo;
 
 public class ArticleListAction implements Action {
 	@Override
 	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) {
-		ArrayList<ArticleVo> articleList = new ArrayList();
-		ArticleVo articleVo = new ArticleVo();
-		articleVo.setArticleNum(1);
-		articleVo.setArticleTitle("테스트중입니다");
-		articleVo.setArticleContent("하하");
-		articleVo.setHit(0);
-		articleVo.setWriteDate("2020-09-22 13:10");
-		articleVo.setUpdateDate("2020-09-22 13:13");
-		articleVo.setDeleteDate("2020-09-22 13:16");
-
-		articleList.add(articleVo);
+		BoardService svc = new BoardService();
 		
-		ArticleVo articleVo1 = new ArticleVo();
-		articleVo.setArticleNum(2);
-		articleVo1.setArticleTitle("테스트중입니다2");
-		articleVo1.setArticleContent("하하2");
-		articleVo1.setHit(10);
-		articleVo1.setWriteDate("2020-09-22 13:10");
-		articleVo1.setUpdateDate("2020-09-22 13:13");
-		articleVo1.setDeleteDate("2020-09-22 13:16");
-		
-		articleList.add(articleVo1);
+		//service의 getArticleList로 리스트 불러오기 떠넘김
+		ArrayList<ArticleVo> articleList = svc.getArticleList();
 		
 		ActionForward forward = new ActionForward();
 		request.setAttribute("list", articleList);
